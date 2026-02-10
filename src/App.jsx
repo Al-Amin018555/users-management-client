b89f58d9a02c0443a843224d07a18dfdaa6e4c06
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-02-10 22:21:20 +0600

    users data fetched from the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Users from './components/users';
+
+const usersPromise = fetch("http://localhost:3000/users").then(res => res.json());
 
 function App() {
-  
+
   return (
     <>
-       <h1>Users management system</h1>
-      
+      <h1>Users management application</h1>
+      <Users usersPromise={usersPromise}></Users>
     </>
   )
 }
---
+++ src/components/Users.jsx
@@ -0,0 +1,13 @@
+import { use } from "react";
+
+const Users = ({ usersPromise }) => {
+    const users = use(usersPromise)
+    console.log(users)
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Users;
